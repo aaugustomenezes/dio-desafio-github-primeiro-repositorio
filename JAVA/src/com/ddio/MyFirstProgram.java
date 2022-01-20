ecba5bf1f35f4c2bda9b10f228c02969a33a6a0d
aaugustomenezes <antonioaugustomenezes@gmail.com>
2022-01-20 19:15:42 -0300

    Anotações Java e Primeiro programa em Java

---
+++ JAVA/src/com/ddio/MyFirstProgram.java
@@ -0,0 +1,11 @@
+package com.ddio;
+
+import com.ddio.base.Order;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		Order order = new Order ("code1234");
+		System.out.println(order);	
+	}
+}
